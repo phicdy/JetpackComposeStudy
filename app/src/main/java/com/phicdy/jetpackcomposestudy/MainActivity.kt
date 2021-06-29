0fb5f5e453abd3d13300a612a80baf1d24d8274e
phicdy <phicdy@gmail.com>
2021-06-29 11:20:38 +0900

    Add NewsStory

--- app/src/main/java/com/phicdy/jetpackcomposestudy/MainActivity.kt
+++ app/src/main/java/com/phicdy/jetpackcomposestudy/MainActivity.kt
@@ -3,36 +3,54 @@ package com.phicdy.jetpackcomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.phicdy.jetpackcomposestudy.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Daveport, California")
+        Text(text = "December 2018")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+    NewsStory()
 }
